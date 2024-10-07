e8b78cca055b15ce2ea22e56b9e8e37c0502372c
nejdbedoui <nejdbedoui@gmail.com>
2024-10-07 13:18:18 +0100

    add home page forgot password verify email

--- UserApp/urls.py
+++ UserApp/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.user_list, name='users_list'),
     path('login', views.user_login, name='login'),
     path('signup', views.signup, name='signup'),
     path('profile', views.profile, name='profile'),
     path('profile/info/', views.personalInfo, name='userInfo'),
     path('logout', views.logout_user, name='logout'),
     path('verifyEmail', views.verifyEmail, name='verify_email'),
+    path('resetPassword', views.reset_password, name='resetPassword'),
+    path('reset_password', views.reset_password_view, name='reset_password'),
 ]
